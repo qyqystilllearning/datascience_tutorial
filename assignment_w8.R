5fc83066624b81ec6fb7fc70a03d4004c68f2a39
qyqystilllearning <capt.rizqy@gmail.com>
2026-07-16 02:09:50 +0800

    data creation

---
+++ assignment_w8.R
@@ -0,0 +1,21 @@
+# Week 8 Assignment
+# Muhammad Rizqy Hidayat - 26000108
+
+# DATAFRAME creation
+
+# Creating the employee_df data frame using the exact data from FIGURE 1
+employee_df <- data.frame(
+  Employee_ID = c("E001", "E002", "E003", "E004", "E005", "E002", "E006", "E007"),
+  Name = c("Sarah", "Omar", "Aisha", "James", "Rita", "Omar", "Khalid", "Nisa"),
+  Department = c("HR", "Sales", "IT", "Marketing", "IT", "Sales", "Finance", "HR"),
+  Monthly_Sales = c(45, 250, 65, 59, 72, 82, -15, 55),
+  Projects_Completed = c(8, 10, 7, NA, 8, 10, 9, NA),
+  Satisfaction_Score = c(7.8, 6.9, NA, 8.2, 7.5, 6.9, 9.1, 12.5)
+)
+
+# View the actual data frame
+View(employee_df)
+
+
+# Output will display the records for E001 and E003
+employee_df[c(1,3), ]
