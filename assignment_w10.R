3104b906d013c757ea22277bf84c6b04b95b3b2c
qyqystilllearning <capt.rizqy@gmail.com>
2026-07-23 21:47:16 +0800

    exercise description for w10

--- assignment_w10.R
+++ assignment_w10.R
@@ -1,3 +1,12 @@
 # Week 10 Assignment
 # Muhammad Rizqy Hidayat - 26000108
 
+# 1. Write a R code to create a sequence of 20 numbers. The program will calculate and
+# display the square of the number sequence.
+
+# 2. Given two variables, num1=0.956786 and num2=7.8345901. Write a R code to
+# display the num1 value in 2 decimal point number, and num2 value in 3 decimal point
+# number (clue: use function round()).
+
+# 3. Write a R code that retrieves from the user the radius value of a circle. The program
+# will calculate and display the circle’s area.
