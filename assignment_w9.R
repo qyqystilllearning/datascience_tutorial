944cd955a4f8cbbb53e1bb4171e521ada1320fb1
qyqystilllearning <capt.rizqy@gmail.com>
2026-07-23 21:31:20 +0800

    Part A asignment 9

--- assignment_w9.R
+++ assignment_w9.R
@@ -1,2 +1,43 @@
 # Week 9 Assignment
 # Muhammad Rizqy Hidayat - 26000108
+
+# ---------- #
+#   PART A   #
+# ---------- #
+
+# 1. Dataset Preparation & Splitting
+df_A <- data.frame(
+  Weight_kg = c(0.5, 0.8, 1.2, 1.6, 2.0, 2.4, 2.8, 3.2, 3.6, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5),
+  Fee_RM = c(6.5, 7.2, 8.0, 8.9, 9.6, 10.3, 11.1, 12.0, 12.8, 13.6, 14.7, 15.8, 16.6, 17.5, 18.4)
+)
+train_A <- df_A[1:10, ]
+test_A <- df_A[11:15, ]
+
+# 2. Fit SLR Model
+model_A <- lm(Fee_RM ~ Weight_kg, data = train_A)
+
+# 3. Report Coefficients
+summary(model_A)
+
+# 4. Predictions & Results Table
+pred_A <- predict(model_A, newdata = test_A)
+results_A <- data.frame(Weight_kg = test_A$Weight_kg, Actual = test_A$Fee_RM, Predicted = round(pred_A, 3))
+
+View(results_A)
+
+# 5. Compute MAPE and RMSE
+error_A <- test_A$Fee_RM - pred_A
+mape_A <- mean(abs(error_A / test_A$Fee_RM)) * 100
+rmse_A <- sqrt(mean(error_A^2))
+
+error_A
+mape_A
+rmse_A
+
+# 6. Plotting
+plot(df_A$Weight_kg, df_A$Fee_RM, main = "Courier Fee vs. Weight",
+     xlab = "Weight (kg)", ylab = "Fee (RM)", pch = 16, col = "darkblue")
+abline(model_A, col = "red", lwd = 2)
+points(test_A$Weight_kg, pred_A, pch = 4, col = "green", cex = 2, lwd = 2)
+legend("topleft", legend = c("Data Points", "Regression Line", "Test Predictions (X)"),
+       pch = c(16, NA, 4), lty = c(NA, 1, NA), col = c("darkblue", "red", "green"))
